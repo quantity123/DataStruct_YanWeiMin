d2efe8fde3654a41f5df3b057ce8d4e83bc722ce
Rayner <283186660@qq.com>
2017-04-07 17:58:07 +0800

    Comm目录

---
+++ Comm/CommDef.h
@@ -0,0 +1,12 @@
+#define True 1
+#define False 0
+#define OK 1
+#define Error 0
+#define Infeasible -1
+#define OverFlow -2
+#define LIST_INIT_SIZE 100	//线性表存储空间的初始分配量
+#define LISTINCREMENT 10	//线性表存储空间的分配增量
+
+typedef int Status;
+typedef int ElemType;
+typedef char SElemType;
